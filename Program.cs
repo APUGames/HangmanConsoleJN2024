ec3268e3abab3b5cdc26e09e44736faef3ed7aaf
Jesus Noland <jesusnoland@gmail.com>
2024-01-16 15:24:02 -0800

    First

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace HangTheMan
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, GAME 135!");
+
+            string lastName = Console.ReadLine();
+            // What will this run?
+            Console.WriteLine("Hi " + lastName);
+        }
+    }
+}
